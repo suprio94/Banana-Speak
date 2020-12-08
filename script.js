7957293cf5185182d5ce5719b30497b9250b9b98
suprio94 <suprio.roy94@gmail.com>
2020-12-08 11:44:39 +0530

    User Input Stored

---
+++ script.js
@@ -0,0 +1,14 @@
+var btnTranslate= document.querySelector("#btn_Translate");
+var txtInput= document.querySelector("#txt_input");
+//console.log(txtInput);
+
+
+
+
+btnTranslate.addEventListener("click", ClickEventHandler)
+
+function ClickEventHandler(){
+
+console.log(txtInput.value);
+}
+
