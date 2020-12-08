4355e9e25a718bd5101bbf1e7c56c4e78da1f67d
suprio94 <suprio.roy94@gmail.com>
2020-12-08 12:42:24 +0530

    InputOutputProcessing

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 var btnTranslate= document.querySelector("#btn_Translate");
+
 var txtInput= document.querySelector("#txt_input");
-//console.log(txtInput);
 
 
+var OutputArea= document.querySelector("[name=OutputText]");
 
+
+var minion = "agkdjghsjkflkjhfhgfh";
 
 btnTranslate.addEventListener("click", ClickEventHandler)
 
 function ClickEventHandler(){
 
-console.log(txtInput.value);
+
+
+var MinOut= minion + " " + txtInput.value;
+
+OutputArea.innerText = MinOut;
 }
 
